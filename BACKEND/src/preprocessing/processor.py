24f34e39fae66df9d8fce81ff1d457ec05337af4
Yassir LAIRGI <yassirlairgi@gmail.com>
2022-10-02 13:33:19 +0200

    fixing bugs in processor class

--- BACKEND/src/preprocessing/processor.py
+++ BACKEND/src/preprocessing/processor.py
@@ -1,4 +1,4 @@
-from dataloader import ImageLoader
+from ..dataloader import ImageLoader
 import numpy as np 
 import cv2 as cv
 from scipy.ndimage import median_filter
@@ -6,18 +6,19 @@ from scipy.ndimage import median_filter
 class ImageProcessor(ImageLoader):
     def __init__(self, id: str):
         super().__init__(id)
+        self.img = self.load()
 
-    def enhance(img, clipLimit = 10.0, window = 50, denoising = True, sharpen=True):
+    def enhance(self, clipLimit = 10.0, window = 50, denoising = True, sharpen=True):
         sharpen_kernel = np.array([[0, -1, 0],
                         [-1, 5, -1],
                         [0, -1, 0]])
 
         clahe = cv.createCLAHE(clipLimit = clipLimit, tileGridSize = (window, window))
 
-        img_result = np.zeros(img.shape)
-        img_result[:,:,0] = clahe.apply(img[:,:,0])
-        img_result[:,:,1] = clahe.apply(img[:,:,1])
-        img_result[:,:,2] = clahe.apply(img[:,:,2])
+        img_result = np.zeros(self.img.shape)
+        img_result[:,:,0] = clahe.apply(self.img[:,:,0])
+        img_result[:,:,1] = clahe.apply(self.img[:,:,1])
+        img_result[:,:,2] = clahe.apply(self.img[:,:,2])
 
         img_result = median_filter(img_result.astype('uint8'), 3)
 
