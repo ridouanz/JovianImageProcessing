d3c66faca19488784baa89aad4838d330f51d802
Ridouan ZAMAH <53916090+ridouanz@users.noreply.github.com>
2022-11-06 00:04:06 +0100

    add image upload API

--- BACKEND/api/test.py
+++ BACKEND/api/test.py
@@ -1,11 +1,13 @@
-from tkinter import Image
-from typing import Union
 import cv2 as cv
 from fastapi import FastAPI
 import pathlib
 import os
 import sys
 from fastapi.middleware.cors import CORSMiddleware
+
+from fastapi import FastAPI, File, Request
+from datetime import datetime
+
 
 parent_path = pathlib.Path(os.path.realpath(__file__)).parent.parent
 sys.path.append(str(parent_path))
@@ -18,8 +20,6 @@ app = FastAPI()
 
 #fix the Cross-Origin Resource Sharing with angular
 origins = [
-    "http://localhost:4200",
-    "http://localhost",
     "*"
 ]
 
@@ -33,15 +33,38 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"msg": "Hello :)"}
 
 
-@app.get("/process/{img_id}")
-def read_item(img_id : str):
+@app.post("/process")
+async def getInformation(info : Request):
+    req_info = await info.json()
+    img_id=req_info["img"]
+    
     img_raw = ImageLoader(img_id).load()
     img_processed = ImageProcessor(img_id).enhance()
-    ##cv.imwrite(f"{str(parent_path)}/data/raw/{img_id}.png", img_raw)
-    ##cv.imwrite(f"{str(parent_path)}/data/processed/{img_id}.png", img_processed)
 
-    cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/dataProcess/{img_id}_edited.png", img_processed)
-    return {"img": f"{img_id}_edited.png"}
+    cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/imgs/{img_id}.png", img_raw)
+    cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/processed_imgs/{img_id}.png", img_processed)
+
+    return {"old": f"{img_id}.png", 
+    "new" : f"{img_id}_Processed.png"
+    }
+
+
+@app.post("/files")
+async def UploadImage(file: bytes = File(...)):
+
+    timestr = datetime.now().strftime("%Y%m%d-%H%M%S")
+    name=timestr+".png"
+
+    with open(f"{str(parent_path)}/FRONTEND/src/assets/imgs/"+str(name),'wb') as image:
+        image.write(file)
+        image.close()
+
+    ##uploaded_processed= ImageProcessor("the path to the picture").enhance()
+    ##cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/processed_imgs/{str(name)}", uploaded_processed)
+
+    return {"old": str(name),
+    "new": str(name)
+    }
