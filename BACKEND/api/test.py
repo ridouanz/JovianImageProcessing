c25033404b1b73c586fa9d8e6207aee514526887
Ridouan ZAMAH <53916090+ridouanz@users.noreply.github.com>
2022-11-06 11:13:30 +0100

    fix the image loading

--- BACKEND/api/test.py
+++ BACKEND/api/test.py
@@ -1,5 +1,5 @@
 import cv2 as cv
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 import pathlib
 import os
 import sys
@@ -7,6 +7,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi import FastAPI, File, Request
 from datetime import datetime
+import numpy as np
 
 
 parent_path = pathlib.Path(os.path.realpath(__file__)).parent.parent
@@ -40,31 +41,35 @@ def read_root():
 async def getInformation(info : Request):
     req_info = await info.json()
     img_id=req_info["img"]
-    
-    img_raw = ImageLoader(img_id).load()
-    img_processed = ImageProcessor(img_id).enhance()
+
+    img_raw = ImageLoader(id = img_id).load()
+    img_processed = ImageProcessor(id = img_id).enhance()
 
     cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/imgs/{img_id}.png", img_raw)
-    cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/processed_imgs/{img_id}.png", img_processed)
+    cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/processed_imgs/{img_id}_processed.png", img_processed)
 
     return {"old": f"{img_id}.png", 
-    "new" : f"{img_id}_Processed.png"
+    "new" : f"{img_id}_processed.png"
     }
 
 
 @app.post("/files")
-async def UploadImage(file: bytes = File(...)):
+async def UploadImage(file: UploadFile = File(...)):
+    name = datetime.now().strftime("%Y%m%d-%H%M%S")
 
-    timestr = datetime.now().strftime("%Y%m%d-%H%M%S")
-    name=timestr+".png"
+    img = await file.read()
+    
+    arr = np.asarray(bytearray(img), dtype=np.uint8)
+    img = cv.imdecode(arr, -1) # 'Load it as it is'
+    img_processed = ImageProcessor(img = img).enhance()
+    """"
+    image_processor = ImageProcessor(img = img)
+    img_processed = image_processor.enhance()
+    raw_img = image_processor.image
+    """
+    cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/imgs/{str(name)}.png", img)
+    cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/processed_imgs/{str(name)}_processed.png", img_processed)
 
-    with open(f"{str(parent_path)}/FRONTEND/src/assets/imgs/"+str(name),'wb') as image:
-        image.write(file)
-        image.close()
-
-    ##uploaded_processed= ImageProcessor("the path to the picture").enhance()
-    ##cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/processed_imgs/{str(name)}", uploaded_processed)
-
-    return {"old": str(name),
-    "new": str(name)
+    return {"old": str(name)+".png",
+    "new": str(name)+"_processed.png"
     }
