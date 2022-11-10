1d4767ab2577a4ef3657109e56069c6b099255c1
Ridouan ZAMAH <53916090+ridouanz@users.noreply.github.com>
2022-11-10 20:59:20 +0100

    edit Dockerfile

--- BACKEND/api/run_all.sh
+++ BACKEND/api/run_all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-gunicorn -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker test:app &> /dev/null
-apachectl -D FOREGROUND &
+gunicorn -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker test:app &
+service apache2 start
+apachectl -D FOREGROUND
 service apache2 status
 echo "Both services are running :D"
 wait 
