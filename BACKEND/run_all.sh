8f612b0ba38879eae418fa135de85ea648ec70a4
Ridouan ZAMAH <53916090+ridouanz@users.noreply.github.com>
2022-11-10 19:41:57 +0100

    edit Dockerfile

--- BACKEND/run_all.sh
+++ BACKEND/run_all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 service apache2 start &
-gunicorn -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker test:app 
+cd /api &
+gunicorn -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker test:app
