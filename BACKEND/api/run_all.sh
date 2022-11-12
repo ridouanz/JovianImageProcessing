51de601a1c94600fd8272bf5f9afe57e7d698afa
Ridouan ZAMAH <53916090+ridouanz@users.noreply.github.com>
2022-11-12 01:19:20 +0100

    add front end to be launched with uvicorn

--- BACKEND/api/run_all.sh
+++ BACKEND/api/run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gunicorn -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker test:app &> /dev/null
+gunicorn -k uvicorn.workers.UvicornWorker test:app &> /dev/null
 service apache2 start
 apachectl -D FOREGROUND &
 wait
--- BACKEND/api/test.py
+++ BACKEND/api/test.py
@@ -11,6 +11,7 @@ import numpy as np
 
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 parent_path = pathlib.Path(os.path.realpath(__file__)).parent.parent
 sys.path.append(str(parent_path))
@@ -20,6 +21,8 @@ from src import ImageLoader, ImageProcessor
 parent_path = pathlib.Path(os.path.realpath(__file__)).parent.parent.parent
 sys.path.append(str(parent_path))
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 #fix the Cross-Origin Resource Sharing with angular
 origins = [
@@ -34,14 +37,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+#@app.get("/")
+#def read_root():
+#    return {"msg": "Hello :)"}
+
+
 @app.get("/")
-def read_root():
-    return {"msg": "Hello :)"}
-
-
-@app.get("/front")
-def read_index():
-    return FileResponse("./index.html")
+def get_app_angular():
+    with open('static/index.html', 'r') as file_index:
+        html_content = file_index.read()
+    return HTMLResponse(html_content, status_code=200)
 
 
 @app.post("/process")
@@ -51,8 +56,11 @@ async def getInformation(info : Request):
     img_raw = ImageLoader(id = img_id).load()
     img_processed = ImageProcessor(id = img_id).enhance()
     
-    cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/imgs/{img_id}.png", img_raw)
-    cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/processed_imgs/{img_id}_processed.png", img_processed)
+    #cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/imgs/{img_id}.png", img_raw)
+    #cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/processed_imgs/{img_id}_processed.png", img_processed)
+
+    cv.imwrite(f"{str(parent_path)}/BACKEND/api/assets/imgs/{img_id}.png", img_raw)
+    cv.imwrite(f"{str(parent_path)}/BACKEND/api/assets/processed_imgs/{img_id}_processed.png", img_processed)
 
     return {"old": f"{img_id}.png", 
     "new" : f"{img_id}_processed.png"
@@ -73,8 +81,11 @@ async def UploadImage(file: UploadFile = File(...)):
     img_processed = image_processor.enhance()
     raw_img = image_processor.image
     
-    cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/imgs/{str(name)}.png", raw_img)
-    cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/processed_imgs/{str(name)}_processed.png", img_processed)
+    #cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/imgs/{str(name)}.png", raw_img)
+    #cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/processed_imgs/{str(name)}_processed.png", img_processed)
+
+    cv.imwrite(f"{str(parent_path)}/BACKEND/api/assets/imgs/{str(name)}.png", raw_img)
+    cv.imwrite(f"{str(parent_path)}/BACKEND/api/assets/processed_imgs/{str(name)}_processed.png", img_processed)
 
     return {"old": str(name)+".png",
     "new": str(name)+"_processed.png"
@@ -87,7 +98,11 @@ async def getInformation(info : Request):
     name = datetime.now().strftime("%Y%m%d-%H%M%S")
     img_raw=cv.cvtColor(cv.imread( img_path ) , cv.COLOR_BGR2RGB)
     img_processed=ImageProcessor.processing_pipeline(img = img_raw,jsn= req_info[1]['params'])
-    cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/processed_imgs/{name}_processed.png", img_processed)
+
+    #cv.imwrite(f"{str(parent_path)}/FRONTEND/src/assets/processed_imgs/{name}_processed.png", img_processed)
+    
+    cv.imwrite(f"{str(parent_path)}/BACKEND/api/assets/processed_imgs/{name}_processed.png", img_processed)
+
     return {"old": req_info[0]['url'],
     "new": f"{name}_processed.png"
     }
